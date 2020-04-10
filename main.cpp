0ac5671b3dc76791995046aa1f31615d92da99f2
luisejv <luisejv@LAPTOP-J7CR1JEP.localdomain>
2020-04-10 16:44:41 -0500

    work done

--- main.cpp
+++ main.cpp
@@ -1,3 +1,35 @@
-int main(){
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+    string line;
+    char quoteOrientation = 'd';
+    while(getline(cin, line)){
+        vector<char> newLine;
+        stringstream ss(line);
+        char path;
+        while (ss >> noskipws >> path){
+            if (path == '"'){
+                if (quoteOrientation == 'd'){
+                    newLine.push_back('`');
+                    newLine.push_back('`');
+                    quoteOrientation = 'u';
+                } else {
+                    newLine.push_back('\'');
+                    newLine.push_back('\'');
+                    quoteOrientation = 'd';
+                }
+            } else {
+                newLine.push_back(path);
+            }
+        }
+        for (auto p : newLine){
+            cout << p;
+        }
+        cout << endl;
+    }
 }
